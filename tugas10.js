32b3e980ceefbfd55d80c3e49c3fb19baf7a6066
Saddam Husein <saddambinhusein2@gmail.com>
2021-11-26 10:47:01 +0800

    Tambahan cara penyelesaian

--- tugas10.js
+++ tugas10.js
@@ -1,6 +1,20 @@
+/*
 let count = 0
 
 for (let x = 1; x <= 10; x++) {
     count = count + x;
     console.log(count)
+}
+*/
+
+// tambahan cara penyelesaian:
+let count = 0;
+
+for (let i = 1; i <= 55; i++) {
+    if (count <= 55 && count !== 0) {
+        console.log(count);
+        count += i;
+    } else if (count === 0) {
+        count += i;
+    }
 }
